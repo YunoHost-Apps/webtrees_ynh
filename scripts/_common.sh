5282bac5b94b8d300008411bd2e3dcc7f3a0ad30
yalh76 <yalh@yahoo.com>
2019-04-18 05:10:05 +0200

    spacing

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="php-gd php-xml mailutils "
+pkg_dependencies="php-gd php-xml mailutils"
 
 #=================================================
 # PERSONAL HELPERS
