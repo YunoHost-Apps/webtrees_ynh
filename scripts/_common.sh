e0e12f0b781dc926ed63c79be35cba2420c47ce7
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-08-22 12:42:38 +0200

    Testing (#35)
    * cleaning (#32)

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml mailutils"
+pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml mailutils"
 
 #=================================================
 # PERSONAL HELPERS
