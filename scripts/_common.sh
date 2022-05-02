18762576d57a03ed03186ce6ced3ddd5ff4a8c89
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-05-02 19:15:41 +0200

    Testing (#43)
    * 2.0.14 (#24)
    
    * 2.0.14
    
    * Update install
    
    * Upgrade to 2.0.15 (#26)
    
    * Upgrade to 2.0.15
    
    * Update README.md
    
    * Upgrade to 2.0.16
    
    * [autopatch] Update issue and PR templates
    
    * Attempt to fix the stupid encoding issue in LICENSE, breaking the linter
    
    * Add php7.3-zip
    
    * cleaning (#32)
    
    * Update install
    
    * Update restore
    
    * fix linter
    
    * 2.0.17 (#36)
    
    * 2.0.17
    
    * Fix
    
    * 2.0.19 (#38)
    
    * 2.0.19
    
    * 2.0.21 (#40)
    
    * 2.0.21
    
    * Auto-update README
    
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>
    
    * 2.0.23
    
    * Auto-update README
    
    * 2.1.0
    
    * Auto-update README
    
    * Update _common.sh
    
    * 2.1.1
    
    * Auto-update README
    
    * Update _common.sh
    
    * Create updater.sh
    
    * Update updater.sh
    
    * Add change_url
    
    * Update check_process
    
    Co-authored-by: Yunohost-Bot <>
    Co-authored-by: Alexandre Aubin <alex.aubin@mailoo.org>
    Co-authored-by: yunohost-bot <yunohost@yunohost.org>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_PHP_VERSION="7.4"
 
 # dependencies used by the app
-pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml mailutils"
+pkg_dependencies="php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-xml mailutils"
 
 #=================================================
 # PERSONAL HELPERS
