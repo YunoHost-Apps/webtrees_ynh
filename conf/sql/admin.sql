96380a2a25261a5c1f3628640a401f067522f389
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-06-11 16:41:26 +0200

    Update admin.sql

--- conf/sql/admin.sql
+++ conf/sql/admin.sql
@@ -1,3 +1,3 @@
-INSERT INTO `wt_user` (`user_id`, `user_name`, `real_name`, `email`, `password`) VALUES (NULL, '__ADMIN__', '__ADMIN__', '__EMAIL__', '__PASSWORD__');
+INSERT INTO `wt_user` (`user_id`, `user_name`, `real_name`, `email`, `password`) VALUES (NULL, '__ADMIN__', '__FULLNAME__', '__EMAIL__', '__PASSWORD__');
 INSERT INTO `wt_user_setting` (`user_id`, `setting_name`, `setting_value`) VALUES ('1', 'canadmin', '1'), ('1', 'verified', '1'), ('1', 'verified_by_admin', '1');
  
